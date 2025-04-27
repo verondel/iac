e8fad760b28198d8653f2be2abc69e1fb31a40bb
Вера Скрыль <vaskryl@sberbank.ru>
2025-04-27 14:08:23 +0300

    [feat] workable services

--- services/main.tf
+++ services/main.tf
@@ -50,101 +50,8 @@ output "gitlab_external_ip" {
 }
 
 // ---------------------------------------------------
-// Фаза 2: cert-manager + ClusterIssuer
+// Фаза 2: gitlab NS + Ingress Nginx for certs
 // ---------------------------------------------------
-
-# Установка CRD до Helm-релиза
-locals {
-  cert_manager_crds = split("\n---\n", file("${path.module}/helm-values/cert-manager.crds.yaml"))
-}
-
-resource "kubernetes_manifest" "cert_manager_crds" {
-  for_each = { for idx, doc in local.cert_manager_crds : idx => yamldecode(doc) }
-
-  manifest = each.value
-}
-
-
-
-# resource "kubernetes_manifest" "cert_manager_crds" {
-#   manifest = yamldecode(file("${path.module}/helm-values/cert-manager.crds.yaml"))
-# }
-# resource "null_resource" "cert_manager_crds" {
-#   provisioner "local-exec" {
-#     command = "kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.2/cert-manager.crds.yaml"
-#   }
-# }
-
-// Устанавливаем cert-manager (управляет TLS-сертификатами через Let's Encrypt)
-resource "helm_release" "cert_manager" {
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  namespace  = "cert-manager"
-
-  create_namespace = true
-
-  // Обязательно устанавливаем CRD (CustomResourceDefinitions)
-  set {
-    name  = "installCRDs"
-    value = "false" # уже применили вручную
-  }
-
-  version = "v1.14.2"
-
-  # depends_on = [null_resource.cert_manager_crds]
-  depends_on = [kubernetes_manifest.cert_manager_crds]
-}
-
-// Создаём ClusterIssuer (сущность, выпускающая сертификаты Let's Encrypt)
-resource "helm_release" "clusterissuer" {
-  name       = "clusterissuer"
-  namespace  = "cert-manager"
-  repository = "https://charts.helm.sh/incubator"
-  chart      = "raw"
-  version    = "0.2.5"
-
-  values = [
-    templatefile("${path.module}/helm-values/letsencrypt-clusterissuer.tpl.yaml", {
-      tls_email = var.tls_email
-    })
-  ]
-}
-
-
-// ---------------------------------------------------
-// Фаза 3: Сертификат Let's Encrypt для GitLab
-// ---------------------------------------------------
-
-# resource "kubernetes_manifest" "gitlab_certificate" {
-#   manifest = {
-#     apiVersion = "cert-manager.io/v1"
-#     kind       = "Certificate"
-#     metadata = {
-#       name      = "gitlab-tls"
-#       namespace = "gitlab"
-#     }
-#     spec = {
-#       secretName = "gitlab-tls"
-#       issuerRef = {
-#         name = "letsencrypt-prod"
-#         kind = "ClusterIssuer"
-#       }
-#       dnsNames = [
-#         "gitlab.${var.domain}",
-#         "registry.${var.domain}",
-#         "minio.${var.domain}"
-#       ]
-#     }
-#   }
-
-#   depends_on = [
-#     helm_release.clusterissuer,
-#     yandex_dns_recordset.gitlab,
-#     yandex_dns_recordset.registry,
-#     yandex_dns_recordset.minio
-#   ]
-# }
 
 resource "kubernetes_namespace" "gitlab" {
   metadata {
@@ -165,6 +72,11 @@ resource "helm_release" "nginx_ingress" {
     name  = "controller.ingressClassResource.name"
     value = "gitlab-nginx"
   }
+  set {
+    name  = "controller.ingressClass"
+    value = "gitlab-nginx"
+  }
+
 
   set {
     name  = "controller.ingressClassResource.enabled"
@@ -185,6 +97,10 @@ resource "helm_release" "nginx_ingress" {
     name  = "controller.service.externalTrafficPolicy"
     value = "Local"
   }
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = yandex_vpc_address.gitlab_ip.external_ipv4_address[0].address
+  }
 
   depends_on = [
     kubernetes_namespace.gitlab
@@ -192,28 +108,59 @@ resource "helm_release" "nginx_ingress" {
 }
 
 
-# resource "helm_release" "gitlab_certificate" {
-#   name       = "gitlab-certificate"
-#   namespace  = "gitlab"
-#   repository = "https://charts.helm.sh/incubator"
-#   chart      = "raw"
-#   version    = "0.2.5"
+// ---------------------------------------------------
+// Фаза 3: cert-manager + ClusterIssuer
+// ---------------------------------------------------
 
-#   values = [
-#     templatefile("${path.module}/helm-values/gitlab-certificate.tpl.yaml", {
-#       domain = var.domain
-#     })
-#   ]
+# Установка CRD до Helm-релиза
+locals {
+  cert_manager_crds = split("\n---\n", file("${path.module}/helm-values/cert-manager.crds.yaml"))
+}
 
-#   depends_on = [
-#     kubernetes_namespace.gitlab,
-#     helm_release.clusterissuer,
-#     kubernetes_namespace.gitlab,
-#     yandex_dns_recordset.gitlab,
-#     yandex_dns_recordset.registry,
-#     yandex_dns_recordset.minio
-#   ]
-# }
+resource "kubernetes_manifest" "cert_manager_crds" {
+  for_each = { for idx, doc in local.cert_manager_crds : idx => yamldecode(doc) }
+
+  manifest = each.value
+}
+
+// Устанавливаем cert-manager (управляет TLS-сертификатами через Let's Encrypt)
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  namespace  = "cert-manager"
+
+  create_namespace = true
+
+  // Обязательно устанавливаем CRD (CustomResourceDefinitions)
+  set {
+    name  = "installCRDs"
+    value = "false" # уже применили вручную
+  }
+
+  version = "v1.14.2"
+
+  # depends_on = [null_resource.cert_manager_crds]
+  depends_on = [
+    kubernetes_manifest.cert_manager_crds,
+    helm_release.nginx_ingress
+  ]
+}
+
+// Создаём ClusterIssuer (сущность, выпускающая сертификаты Let's Encrypt)
+resource "helm_release" "clusterissuer" {
+  name       = "clusterissuer"
+  namespace  = "cert-manager"
+  repository = "https://charts.helm.sh/incubator"
+  chart      = "raw"
+  version    = "0.2.5"
+
+  values = [
+    templatefile("${path.module}/helm-values/letsencrypt-clusterissuer.tpl.yaml", {
+      tls_email = var.tls_email
+    })
+  ]
+}
 
 
 resource "local_file" "gitlab_cert_manifest" {
@@ -232,7 +179,7 @@ resource "null_resource" "apply_gitlab_cert" {
   ]
 
   provisioner "local-exec" {
-    command = "kubectl apply -f ${local_file.gitlab_cert_manifest.filename}"
+    command = "kubectl apply -f ${local_file.gitlab_cert_manifest.filename} --kubeconfig=vera-infra-kubeconfig.yaml"
   }
 
   triggers = {
@@ -244,6 +191,9 @@ resource "null_resource" "wait_cert_ready" {
   depends_on = [null_resource.apply_gitlab_cert]
 
   provisioner "local-exec" {
+    environment = {
+      KUBECONFIG = "vera-infra-kubeconfig.yaml"
+    }
     command     = <<EOT
       echo "⏳ Ждём, пока сертификат gitlab-tls будет готов..."
       for i in {1..30}; do
@@ -265,8 +215,9 @@ resource "null_resource" "wait_cert_ready" {
 
 
 
+
 // ---------------------------------------------------
-// Фаза 4: Установка GitLab
+// Фаза 5: Установка GitLab
 // ---------------------------------------------------
 
 resource "helm_release" "gitlab" {
